1fd7b677a04c478d99089e57f8d27cef28b93542
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-01-26 12:39:35 +0400

    Adjust DesktopConnectionModalContent design.

--- src/action/containers/Modal/DesktopConnectionModalContent/index.jsx
+++ src/action/containers/Modal/DesktopConnectionModalContent/index.jsx
@@ -4,7 +4,6 @@ import { t } from '@lingui/core/macro'
 import { Trans } from '@lingui/react/macro'
 
 import { ButtonLittle } from '../../../../shared/components/ButtonLittle'
-import { WelcomePageWrapper } from '../../../../shared/components/WelcomePageWrapper'
 import { useModal } from '../../../../shared/context/ModalContext'
 import { SyncIcon } from '../../../../shared/icons/SyncIcon'
 import { logger } from '../../../../shared/utils/logger'
@@ -47,30 +46,34 @@ export const DesktopConnectionModalContent = ({ onRetry, onClose }) => {
     }
   }
   return (
-    <WelcomePageWrapper>
-      <div className="flex h-full w-full flex-col items-center justify-center gap-[20px]">
-        <span className="font-inter text-white-mode1 w-[310] py-[9] text-center text-[20px] leading-[24px] font-bold">
+    <div className="bg-grey500-mode1 relative flex h-full w-full flex-col items-center justify-center overflow-hidden p-9 pt-[52px]">
+      <img
+        src="/assets/images/logoLock.png"
+        className="absolute top-[52px] left-1/2 h-[50px] w-[190px] -translate-x-1/2"
+      />
+
+      <div className="z-10 flex w-full flex-col items-center gap-[20px]">
+        <span className="font-inter text-white-mode1 w-[350] py-[9] text-center text-[20px] leading-[24px] font-bold">
           <Trans>
             Open the desktop app and activate browser extension to continue
           </Trans>
         </span>
 
-        <div className="w-full">
-          <ButtonLittle
-            fullWidth
-            className={`py-[9px] ${loading ? 'cursor-wait' : ''} w-full justify-center`}
-            startIcon={SyncIcon}
-            onClick={handleRetry}
-            disabled={loading}
-          >
-            {loading ? (
-              <Trans>{t`Syncing...`}</Trans>
-            ) : (
-              <Trans>{t`Sync data with desktop app`}</Trans>
-            )}
-          </ButtonLittle>
-        </div>
+        <ButtonLittle
+          className={`w-full py-[9px] ${loading ? 'cursor-wait' : ''} justify-center`}
+          startIcon={SyncIcon}
+          onClick={handleRetry}
+          disabled={loading}
+        >
+          {loading ? (
+            <Trans>{t`Syncing...`}</Trans>
+          ) : (
+            <Trans>{t`Sync data with desktop app`}</Trans>
+          )}
+        </ButtonLittle>
       </div>
-    </WelcomePageWrapper>
+
+      <div className="bg-primary400-mode1 absolute bottom-0 left-1/2 z-[1] h-1/16 w-[100%] translate-x-[-50%] translate-y-[95%] rounded-2xl opacity-70 blur-[50px]" />
+    </div>
   )
 }
