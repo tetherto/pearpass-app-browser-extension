bbe35967c5f7c11d56aa53538fc6d1d9ffe90092
Giorgi Shavtvalishvili <gshavtvalishvili@gmail.com>
2026-03-13 17:14:04 +0400

    Restore debugMode: false, add test for otp.test.ts.

---
+++ src/shared/utils/otp.test.ts
@@ -0,0 +1,13 @@
+import { colors } from 'pearpass-lib-ui-theme-provider'
+
+import { getTimerColor } from './otp'
+
+describe('getTimerColor', () => {
+  it('should return errorRed when expiring is true', () => {
+    expect(getTimerColor(true)).toBe(colors.errorRed.mode1)
+  })
+
+  it('should return primary400 when expiring is false', () => {
+    expect(getTimerColor(false)).toBe(colors.primary400.mode1)
+  })
+})
